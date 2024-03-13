c0050fba6fb62cb4008d9915d193090e08a6a628
KhanhNH <nguyenhuukhanh0701@gmail.com>
2024-03-13 21:58:11 +0700

    build_project

---
+++ src/components/AppBar/index.jsx
@@ -0,0 +1,21 @@
+import Box from '@mui/material/Box'
+import DarkMode from '../../components/DarkMode'
+function AppBar() {
+  return (
+    <>
+      <Box
+        sx={{
+          backgroundColor: 'primary.light',
+          width: '100%',
+          height: (theme) => theme.trello.appBarHeight,
+          display: 'flex',
+          alignItems: 'center',
+        }}
+      >
+        <DarkMode />
+      </Box>
+    </>
+  )
+}
+
+export default AppBar
