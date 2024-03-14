afc30cb92e95826e69f160975305d845b1f100ab
KhanhNH <nguyenhuukhanh0701@gmail.com>
2024-03-14 22:24:22 +0700

    appbar

--- src/components/AppBar/index.jsx
+++ src/components/AppBar/index.jsx
@@ -1,18 +1,59 @@
 import Box from '@mui/material/Box'
 import DarkMode from '~/components/DarkMode'
+import AppsIcon from '@mui/icons-material/Apps'
+import { ReactComponent as logoTrello } from '~/assets/logoTrello.svg'
+import { Badge, Button, SvgIcon, TextField, Tooltip } from '@mui/material'
+import { Typography } from '@mui/material'
+import WorkSpaces from './Menus/WorkSpaces'
+import Recent from './Menus/Recent'
+import Starred from './Menus/Starred'
+import Templates from './Menus/Templates'
+import NotificationsIcon from '@mui/icons-material/Notifications'
+import HelpIcon from '@mui/icons-material/Help'
+import Profile from './Menus/Profile'
+
 function AppBar() {
   return (
     <>
       <Box
+        px={2}
         sx={{
-          backgroundColor: 'primary.light',
           width: '100%',
           height: (theme) => theme.trello.appBarHeight,
           display: 'flex',
           alignItems: 'center',
+          justifyContent: 'space-between',
         }}
       >
-        <DarkMode />
+        <Box sx={{ display: 'flex', alignItems: 'center', gap: 2 }}>
+          <AppsIcon sx={{ color: 'primary.main' }} />
+          <Box sx={{ display: 'flex', alignItems: 'center', gap: 0.5 }}>
+            <SvgIcon component={logoTrello} inheritViewBox sx={{ color: 'primary.main' }} />
+            <Typography variant="span" sx={{ fontSize: '1.2rem', fontWeight: 'bold', color: 'primary.main' }}>
+              Trello
+            </Typography>
+          </Box>
+          <WorkSpaces />
+          <Recent />
+          <Starred />
+          <Templates />
+          <Button variant="outlined">Create</Button>
+        </Box>
+        <Box sx={{ display: 'flex', alignItems: 'center', gap: 1 }}>
+          <TextField id="outlined-search" label="Search..." type="search" size="small" />
+          <DarkMode />
+          <Tooltip title="Notifications" sx={{ mr: 1 }}>
+            <Badge badgeContent={17} color="secondary">
+              <NotificationsIcon />
+            </Badge>
+          </Tooltip>
+          <Tooltip title="Help">
+            <Badge badgeContent={17} color="secondary">
+              <HelpIcon />
+            </Badge>
+          </Tooltip>
+          <Profile />
+        </Box>
       </Box>
     </>
   )
